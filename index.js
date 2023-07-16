3eabbf58666207c6d123d7d5efca2dcc52a4e5af
Bangza1170 <101542013+Bangza1170@users.noreply.github.com>
2023-07-17 02:32:09 +0700

    :guitar: bubble test

--- index.js
+++ index.js
@@ -22,42 +22,39 @@ app.post("/webhook", function (req, res) {
       replyToken: req.body.events[0].replyToken,
       messages: [
         {
-          "type": "template",
-          "altText": "This is a buttons template",
-          "template": {
-            "type": "buttons",
-            "thumbnailImageUrl": "https://picsum.photos/200/300",
-            "imageAspectRatio": "rectangle",
-            "imageSize": "cover",
-            "imageBackgroundColor": "#FFFFFF",
-            "title": "Menu",
-            "text": "Please select",
-            "defaultAction": {
+          "type": "buttons",
+          "thumbnailImageUrl": "https://picsum.photos/200/300",
+          "imageAspectRatio": "rectangle",
+          "imageSize": "cover",
+          "imageBackgroundColor": "#FFFFFF",
+          "title": "Menu",
+          "text": "Please select",
+          "defaultAction": {
+            "type": "uri",
+            "label": "View detail",
+            "uri": "https://picsum.photos/200/300"
+          },
+          "actions": [
+            {
+              "type": "postback",
+              "label": "Buy",
+              "data": "action=buy&itemid=123"
+            },
+            {
+              "type": "postback",
+              "label": "Add to cart",
+              "data": "action=add&itemid=123"
+            },
+            {
               "type": "uri",
               "label": "View detail",
               "uri": "https://picsum.photos/200/300"
-            },
-            "actions": [
-              {
-                "type": "postback",
-                "label": "Buy",
-                "data": "action=buy&itemid=123"
-              },
-              {
-                "type": "postback",
-                "label": "Add to cart",
-                "data": "action=add&itemid=123"
-              },
-              {
-                "type": "uri",
-                "label": "View detail",
-                "uri": "https://picsum.photos/200/300"
-              }
-            ]
-          }
+            }
+          ]
         }
       ]
-    })
+    }
+    )
 
     // Request header
     const headers = {
