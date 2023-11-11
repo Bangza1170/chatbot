f6e9cf826e85727cd053d16a53b84c3484927342
Bangza1170 <101542013+Bangza1170@users.noreply.github.com>
2023-11-11 08:55:22 +0700

    update flex message

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ app.post("/webhook", async function (req, res) {
     req.body.events[0].message.text === "ตารางคะแนน"
   ) {
     try {
-      var listdata = await axios.get("https://cfgn3khv-1412.asse.devtunnels.ms");
-      // var listdata = await axios.get("https://chatbot-b07k.onrender.com");
+      // var listdata = await axios.get("https://cfgn3khv-1412.asse.devtunnels.ms");
+      var listdata = await axios.get("https://nongfootball.onrender.com");
     } catch (error) {
       console.log("axios error: ", error)
     }
