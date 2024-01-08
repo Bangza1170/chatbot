871c92b4b65bbd17237e890967fc4f429b6d5a3a
Bangza1170 <101542013+Bangza1170@users.noreply.github.com>
2024-01-09 00:35:00 +0700

    Revert "chatbotbardAI"
    This reverts commit 2509a47e97668601b62c33b473f9ced50e465401.

--- Service/postBardAi.js
+++ Service/postBardAi.js
@@ -1,23 +1,21 @@
 const TOKENBARD = process.env.POST_GENERAT_KEY_BARD;
 const { default: axios } = require("axios");
 
-import axios from "axios";
-import https from "https";
 
-async function generateBardAIResponse(question) {
+async function generateGoogleBardAIResponse(question) {
   try {
-    let data = JSON.stringify({
+    const data = JSON.stringify({
       prompt: {
         text: question,
       },
     });
 
-    let config = {
-      method: "post",
+    const config = {
+      method: 'post',
       maxBodyLength: Infinity,
       url: `https://generativelanguage.googleapis.com/v1beta2/models/text-bison-001:generateText?key=${TOKENBARD}`,
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       data: data,
     };
@@ -25,120 +23,32 @@ async function generateBardAIResponse(question) {
     const response = await axios.request(config);
     return response.data.candidates[0].output;
   } catch (error) {
-    console.log("generateBardAIResponse error: ", error);
+    console.log('generateGoogleBardAIResponse error: ', error);
     return null;
   }
 }
 
-async function sendLineMessage(replyToken, messages) {
+async function fetchAndProcessData(req) {
   try {
-    const dataString = JSON.stringify({
-      replyToken: replyToken,
-      messages: messages,
-    });
+    const listData = await axios.get('https://gaps-side-approaches-cuba.trycloudflare.com');
+    const data = listData.data.data;
 
-    const headers = {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${TOKEN}`,
-    };
+    const newDataScore = createNewDataScore(data);
+    const dataString = createDataString(req.body.events[0].replyToken, newDataScore);
 
-    const webhookOptions = {
-      hostname: "api.line.me",
-      path: "/v2/bot/message/reply",
-      method: "POST",
-      headers: headers,
-      body: dataString,
-    };
-
-    const request = https.request(webhookOptions, (res) => {
-      res.on("data", (d) => {
-        process.stdout.write(d);
-      });
-    });
-
-    request.on("error", (err) => {
-      console.error(err);
-    });
-
-    request.write(dataString);
-    request.end();
+    await sendLineMessage(dataString);
   } catch (error) {
-    console.log("sendLineMessage error: ", error);
+    console.log('fetchAndProcessData error: ', error);
   }
 }
-
 
 function createNewDataScore(data) {
   const newDataScore = [
     {
-      type: "box",
-      layout: "baseline",
+      type: 'box',
+      layout: 'baseline',
       contents: [
-        {
-          type: "text",
-          text: "Pos.",
-          size: "xxs",
-          weight: "bold",
-          flex: 2,
-        },
-        {
-          type: "text",
-          text: "L",
-          flex: 1,
-          size: "xxs",
-          weight: "bold",
-        },
-        {
-          type: "text",
-          text: "Team",
-          flex: 3,
-          size: "xxs",
-          weight: "bold",
-          margin: "md",
-        },
-        {
-          type: "text",
-          text: "P",
-          flex: 2,
-          size: "xxs",
-          weight: "bold",
-          align: "center",
-        },
-        {
-          type: "text",
-          text: "W",
-          flex: 1,
-          size: "xxs",
-          weight: "bold",
-        },
-        {
-          type: "text",
-          text: "D",
-          flex: 1,
-          size: "xxs",
-          weight: "bold",
-        },
-        {
-          type: "text",
-          text: "L",
-          flex: 1,
-          size: "xxs",
-          weight: "bold",
-        },
-        {
-          type: "text",
-          text: "+/-",
-          size: "xxs",
-          weight: "bold",
-          flex: 2,
-        },
-        {
-          type: "text",
-          text: "Pt",
-          size: "xxs",
-          weight: "bold",
-          flex: 1,
-        },
+        // ... (contents definition)
       ],
     },
   ];
@@ -150,439 +60,77 @@ function createNewDataScore(data) {
   return newDataScore;
 }
 
-function createDataMessage(replyToken, newDataScore) {
-  return {
-    type: "flex",
-    altText: "ตารางคะแนนพรีเมียร์ลีคปัจจุบัน",
-    contents:[
+function createDataString(replyToken, newDataScore) {
+  const dataString = JSON.stringify({
+    replyToken: replyToken,
+    messages: [
       {
-        type: "flex",
-        altText: "ตารางคะแนนพรีเมียร์ลีคปัจจุบัน",
+        type: 'flex',
+        altText: 'ตารางคะแนนพรีเมียร์ลีคปัจจุบัน',
         contents: {
-          type: "bubble",
-          hero: {
-            type: "box",
-            layout: "vertical",
-            contents: [
-              {
-                type: "image",
-                url: "https://ga.lnwfile.com/_/ga/_raw/e2/zk/v9.png",
-                size: "full",
-                aspectRatio: "15:10",
-              },
-            ],
-          },
-          body: {
-            type: "box",
-            layout: "vertical",
-            contents: [
-              {
-                type: "box",
-                layout: "vertical",
-                margin: "lg",
-                spacing: "sm",
-                contents: newDataScore,
-              },
-            ],
-          },
-
-          footer: {
-            type: "box",
-            layout: "vertical",
-            contents: [
-              {
-                type: "button",
-                action: {
-                  type: "uri",
-                  label: "ตารางคะแนนลีคอื่นๆ",
-                  uri: "https://footballline.000webhostapp.com/point.html",
-                },
-                color: "#6600FF",
-                gravity: "center",
-                style: "primary",
-              },
-            ],
-
-            // newDataScore
-          },
+          // ... (contents definition)
         },
       },
-    ]
-  };
+    ],
+  });
+
+  return dataString;
 }
 
-async function postGoogleBardAI(req, res, question) {
-  res.send("HTTP POST request sent to the webhook URL!");
+async function sendLineMessage(dataString) {
+  try {
+    const headers = {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${TOKEN}`,
+    };
 
-  if (question.includes("How To")) {
-    const responseText = await generateBardAIResponse(question);
+    const webhookOptions = {
+      hostname: 'api.line.me',
+      path: '/v2/bot/message/reply',
+      method: 'POST',
+      headers: headers,
+      body: dataString,
+    };
 
-    if (responseText) {
-      const messages = [
-        {
-          type: "text",
-          text: responseText,
-        },
-      ];
+    const request = https.request(webhookOptions, (res) => {
+      res.on('data', (d) => {
+        process.stdout.write(d);
+      });
+    });
 
-      await sendLineMessage(req.body.events[0].replyToken, messages);
-    }
-  } else if (
-    req.body.events[0].message.type === "text" &&
-    req.body.events[0].message.text === "ตารางคะแนน"
-  ) {
-    try {
-      const listData = await axios.get(
-        "https://gaps-side-approaches-cuba.trycloudflare.com"
-      );
-      const data = listData.data.data;
+    request.on('error', (err) => {
+      console.error(err);
+    });
 
-      const newDataScore = createNewDataScore(data);
-      const messages = [
-        createDataMessage(req.body.events[0].replyToken, newDataScore),
-      ];
-
-      await sendLineMessage(req.body.events[0].replyToken, messages);
-    } catch (error) {
-      console.log("axios error: ", error);
-    }
+    request.write(dataString);
+    request.end();
+  } catch (error) {
+    console.log('sendLineMessage error: ', error);
   }
 }
+
+ async function postGoogleBardAI(req, res,question) {
+  if (question.includes('How To')) {
+    const responseText = await generateGoogleBardAIResponse(question);
+    
+    if (responseText) {
+      const dataString = JSON.stringify({
+        replyToken: req.body.events[0].replyToken,
+        messages: [
+          {
+            type: 'text',
+            text: responseText,
+          },
+        ],
+      });
+      await sendLineMessage(dataString);
+    }
+  } else if (req.body.events[0].message.type === 'text' && req.body.events[0].message.text === 'ตารางคะแนน') {
+    await fetchAndProcessData(req);
+  }
+}
+
 module.exports = {
-  postGoogleBardAI,
-};
+  postGoogleBardAI
+}
 
-
-
-
-
-// export async function postGoogleBardAI(req, res, question) {
-//   var dataString = {};
-//   if (message.includes("How To")) {
-//     try {
-//       let data = JSON.stringify({
-//         prompt: {
-//           text: question,
-//         },
-//       });
-
-//       let config = {
-//         method: "post",
-//         maxBodyLength: Infinity,
-//         url: `https://generativelanguage.googleapis.com/v1beta2/models/text-bison-001:generateText?key=${TOKENBARD}`,
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         data: data,
-//       };
-
-//       axios
-//         .request(config)
-//         .then((response) => {
-//           dataString = JSON.stringify({
-//             replyToken: req.body.events[0].replyToken,
-//             messages: [
-//               {
-//                 type: "text",
-//                 text: response.data.candidates[0].output,
-//               },
-//             ],
-//           });
-
-//           try {
-//             const headers = {
-//               "Content-Type": "application/json",
-//               Authorization:
-//                 "Bearer gpW6aqfrVCoBAyhSvPjIZoYYnOYfqYC/JhOSAXMVdYNpAtMOwf+o53maASzmQr0a8wQQTb8SEw3odehXybm7Cw2AfYzcBOqoHFWwJhKhKTzmTxSR0OOZbkA6t2gfnzaQS5w1GPjIG1pmLXRpw199agdB04t89/1O/w1cDnyilFU=",
-//             };
-
-//             // Options to pass into the request
-//             const webhookOptions = {
-//               hostname: "api.line.me",
-//               path: "/v2/bot/message/reply",
-//               method: "POST",
-//               headers: headers,
-//               body: dataString,
-//             };
-
-//             // Define request
-//             const request = https.request(webhookOptions, (res) => {
-//               res.on("data", (d) => {
-//                 process.stdout.write(d);
-//               });
-//             });
-
-//             // Handle error
-//             request.on("error", (err) => {
-//               console.error(err);
-//             });
-
-//             // Send data
-//             request.write(dataString);
-//             request.end();
-//           } catch (error) {
-//             console.log("error reply: ", error);
-//           }
-//         })
-//         .catch((error) => {
-//           console.log(error);
-//         });
-//     } catch (error) {
-//       console.log("axios error: ", error);
-//     }
-//   } else if (
-//     req.body.events[0].message.type === "text" &&
-//     req.body.events[0].message.text === "ตารางคะแนน"
-//   ) {
-//     try {
-//       var listData = await axios.get(
-//         "https://gaps-side-approaches-cuba.trycloudflare.com"
-//       );
-//     } catch (error) {
-//       console.log("axios error: ", error);
-//     }
-//     console.log(listData.data.data);
-//     const newDataScore = [
-//       {
-//         type: "box",
-//         layout: "baseline",
-//         contents: [
-//           {
-//             type: "text",
-//             text: "Pos.",
-//             size: "xxs",
-//             weight: "bold",
-//             flex: 2,
-//           },
-//           {
-//             type: "text",
-//             text: "L",
-//             flex: 1,
-//             size: "xxs",
-//             weight: "bold",
-//           },
-//           {
-//             type: "text",
-//             text: "Team",
-//             flex: 3,
-//             size: "xxs",
-//             weight: "bold",
-//             margin: "md",
-//           },
-//           {
-//             type: "text",
-//             text: "P",
-//             flex: 2,
-//             size: "xxs",
-//             weight: "bold",
-//             align: "center",
-//           },
-//           {
-//             type: "text",
-//             text: "W",
-//             flex: 1,
-//             size: "xxs",
-//             weight: "bold",
-//           },
-//           {
-//             type: "text",
-//             text: "D",
-//             flex: 1,
-//             size: "xxs",
-//             weight: "bold",
-//           },
-//           {
-//             type: "text",
-//             text: "L",
-//             flex: 1,
-//             size: "xxs",
-//             weight: "bold",
-//           },
-//           {
-//             type: "text",
-//             text: "+/-",
-//             size: "xxs",
-//             weight: "bold",
-//             flex: 2,
-//           },
-//           {
-//             type: "text",
-//             text: "Pt",
-//             size: "xxs",
-//             weight: "bold",
-//             flex: 1,
-//           },
-//         ],
-//       },
-//     ];
-//     const data = listData.data.data;
-
-//     for (let i = 0; i < data.length; i++) {
-//       number = i + 1;
-//       let dataScore = {
-//         type: "box",
-//         layout: "baseline",
-//         spacing: "sm",
-//         contents: [
-//           {
-//             type: "text",
-//             text: `${number}`,
-//             color: "#000000",
-//             size: "xxs",
-//             flex: 2,
-//           },
-//           {
-//             type: "icon",
-//             url: data[i].icon,
-//             size: "xxs",
-//           },
-//           {
-//             type: "text",
-//             text: data[i].team,
-//             wrap: true,
-//             color: "#666666",
-//             size: "xxs",
-//             flex: 3,
-//           },
-//           {
-//             type: "text",
-//             text: data[i].pi,
-//             flex: 2,
-//             size: "xxs",
-//             margin: "xl",
-//             align: "center",
-//           },
-//           {
-//             type: "text",
-//             text: data[i].w,
-//             flex: 1,
-//             size: "xxs",
-//             color: "#01B54C",
-//           },
-//           {
-//             type: "text",
-//             text: data[i].d,
-//             flex: 1,
-//             size: "xxs",
-//             color: "#929684",
-//             margin: "none",
-//           },
-//           {
-//             type: "text",
-//             text: data[i].l,
-//             flex: 1,
-//             size: "xxs",
-//             color: "#FA1001",
-//             margin: "none",
-//           },
-//           {
-//             type: "text",
-//             text: data[i].gd,
-//             flex: 2,
-//             size: "xxs",
-//             color: "#000000",
-//           },
-//           {
-//             type: "text",
-//             text: data[i].pts,
-//             flex: 1,
-//             size: "xxs",
-//             color: "#000000",
-//           },
-//         ],
-//       };
-//       newDataScore.push(dataScore);
-//     }
-//     // Message data, must be stringified
-//     const dataString = JSON.stringify({
-//       replyToken: req.body.events[0].replyToken,
-//       messages: [
-//         {
-//           type: "flex",
-//           altText: "ตารางคะแนนพรีเมียร์ลีคปัจจุบัน",
-//           contents: {
-//             type: "bubble",
-//             hero: {
-//               type: "box",
-//               layout: "vertical",
-//               contents: [
-//                 {
-//                   type: "image",
-//                   url: "https://ga.lnwfile.com/_/ga/_raw/e2/zk/v9.png",
-//                   size: "full",
-//                   aspectRatio: "15:10",
-//                 },
-//               ],
-//             },
-//             body: {
-//               type: "box",
-//               layout: "vertical",
-//               contents: [
-//                 {
-//                   type: "box",
-//                   layout: "vertical",
-//                   margin: "lg",
-//                   spacing: "sm",
-//                   contents: newDataScore,
-//                 },
-//               ],
-//             },
-
-//             footer: {
-//               type: "box",
-//               layout: "vertical",
-//               contents: [
-//                 {
-//                   type: "button",
-//                   action: {
-//                     type: "uri",
-//                     label: "ตารางคะแนนลีคอื่นๆ",
-//                     uri: "https://footballline.000webhostapp.com/point.html",
-//                   },
-//                   color: "#6600FF",
-//                   gravity: "center",
-//                   style: "primary",
-//                 },
-//               ],
-
-//               // newDataScore
-//             },
-//           },
-//         },
-//       ],
-//     });
-
-//     console.log("show data_string: ", dataString);
-
-//     // Request header
-//     const headers = {
-//       "Content-Type": "application/json",
-//       Authorization: "Bearer " + TOKEN,
-//     };
-
-//     // Options to pass into the request
-//     const webhookOptions = {
-//       hostname: "api.line.me",
-//       path: "/v2/bot/message/reply",
-//       method: "POST",
-//       headers: headers,
-//       body: dataString,
-//     };
-
-//     // Define request
-//     const request = https.request(webhookOptions, (res) => {
-//       res.on("data", (d) => {
-//         process.stdout.write(d);
-//       });
-//     });
-
-//     // Handle error
-//     request.on("error", (err) => {
-//       console.error(err);
-//     });
-
-//     // Send data
-//     request.write(dataString);
-//     request.end();
-//   }
-// }
