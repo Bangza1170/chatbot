5f13114b0cb8565ce88aac27880f7184cd2a3ec5
Bangza1170 <101542013+Bangza1170@users.noreply.github.com>
2024-01-09 10:25:22 +0700

    chatbotbardAI

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const PORT = process.env.PORT || 3000;
 const TOKENBARD = process.env.POST_GENERAT_KEY_BARD;
 const TOKEN = process.env.LINE_ACCESS_TOKEN;
 
-
 app.use(express.json());
 
 // Function to handle "How To" message
@@ -269,4 +268,7 @@ app.post("/webhook", async function (req, res) {
   }
 
   res.sendStatus(200);
+});
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
 });
