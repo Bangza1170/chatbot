55d02b26c7b9da6c2d11f07a76f3a60e6174e2e7
Bangza1170 <101542013+Bangza1170@users.noreply.github.com>
2024-01-09 15:32:39 +0700

    Chatbot Ai refecter

--- index.js
+++ index.js
@@ -37,7 +37,157 @@ app.post("/webhook", async function (req, res) {
       console.log("axios error: ", error);
     }
     console.log(listData.data.data);
-    createNewDataScore();
+    const newDataScore = [
+      {
+        type: "box",
+        layout: "baseline",
+        contents: [
+          {
+            type: "text",
+            text: "Pos.",
+            size: "xxs",
+            weight: "bold",
+            flex: 2,
+          },
+          {
+            type: "text",
+            text: "L",
+            flex: 1,
+            size: "xxs",
+            weight: "bold",
+          },
+          {
+            type: "text",
+            text: "Team",
+            flex: 3,
+            size: "xxs",
+            weight: "bold",
+            margin: "md",
+          },
+          {
+            type: "text",
+            text: "P",
+            flex: 2,
+            size: "xxs",
+            weight: "bold",
+            align: "center",
+          },
+          {
+            type: "text",
+            text: "W",
+            flex: 1,
+            size: "xxs",
+            weight: "bold",
+          },
+          {
+            type: "text",
+            text: "D",
+            flex: 1,
+            size: "xxs",
+            weight: "bold",
+          },
+          {
+            type: "text",
+            text: "L",
+            flex: 1,
+            size: "xxs",
+            weight: "bold",
+          },
+          {
+            type: "text",
+            text: "+/-",
+            size: "xxs",
+            weight: "bold",
+            flex: 2,
+          },
+          {
+            type: "text",
+            text: "Pt",
+            size: "xxs",
+            weight: "bold",
+            flex: 1,
+          },
+        ],
+      },
+    ];
+    const data = listData.data.data;
+
+    for (let i = 0; i < data.length; i++) {
+      number = i + 1;
+      let dataScore = {
+        type: "box",
+        layout: "baseline",
+        spacing: "sm",
+        contents: [
+          {
+            type: "text",
+            text: `${number}`,
+            color: "#000000",
+            size: "xxs",
+            flex: 2,
+          },
+          {
+            type: "icon",
+            url: data[i].icon,
+            size: "xxs",
+          },
+          {
+            type: "text",
+            text: data[i].team,
+            wrap: true,
+            color: "#666666",
+            size: "xxs",
+            flex: 3,
+          },
+          {
+            type: "text",
+            text: data[i].pi,
+            flex: 2,
+            size: "xxs",
+            margin: "xl",
+            align: "center",
+          },
+          {
+            type: "text",
+            text: data[i].w,
+            flex: 1,
+            size: "xxs",
+            color: "#01B54C",
+          },
+          {
+            type: "text",
+            text: data[i].d,
+            flex: 1,
+            size: "xxs",
+            color: "#929684",
+            margin: "none",
+          },
+          {
+            type: "text",
+            text: data[i].l,
+            flex: 1,
+            size: "xxs",
+            color: "#FA1001",
+            margin: "none",
+          },
+          {
+            type: "text",
+            text: data[i].gd,
+            flex: 2,
+            size: "xxs",
+            color: "#000000",
+          },
+          {
+            type: "text",
+            text: data[i].pts,
+            flex: 1,
+            size: "xxs",
+            color: "#000000",
+          },
+        ],
+      };
+      newDataScore.push(dataScore);
+    }
     // Message data, must be stringified
     const dataString = JSON.stringify({
       replyToken: req.body.events[0].replyToken,
