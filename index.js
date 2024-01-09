77cd594b412146b0a76844cf6de12f6be8a1dc76
Bangza1170 <101542013+Bangza1170@users.noreply.github.com>
2024-01-09 15:10:54 +0700

    Chatbot Ai refecter

--- index.js
+++ index.js
@@ -25,261 +25,111 @@ app.post("/webhook", async function (req, res) {
   if (message.includes("How To")) {
     handelHowToMessage(req, res,message,dataString);
   }
-  // else if (
-  //   req.body.events[0].message.type === "text" &&
-  //   req.body.events[0].message.text === "ตารางคะแนน"
-  // ) {
-  //   try {
-  //     var listData = await axios.get(
-  //       "https://gaps-side-approaches-cuba.trycloudflare.com"
-  //     );
-  //   } catch (error) {
-  //     console.log("axios error: ", error);
-  //   }
-  //   console.log(listData.data.data);
-  //   const newDataScore = [
-  //     {
-  //       type: "box",
-  //       layout: "baseline",
-  //       contents: [
-  //         {
-  //           type: "text",
-  //           text: "Pos.",
-  //           size: "xxs",
-  //           weight: "bold",
-  //           flex: 2,
-  //         },
-  //         {
-  //           type: "text",
-  //           text: "L",
-  //           flex: 1,
-  //           size: "xxs",
-  //           weight: "bold",
-  //         },
-  //         {
-  //           type: "text",
-  //           text: "Team",
-  //           flex: 3,
-  //           size: "xxs",
-  //           weight: "bold",
-  //           margin: "md",
-  //         },
-  //         {
-  //           type: "text",
-  //           text: "P",
-  //           flex: 2,
-  //           size: "xxs",
-  //           weight: "bold",
-  //           align: "center",
-  //         },
-  //         {
-  //           type: "text",
-  //           text: "W",
-  //           flex: 1,
-  //           size: "xxs",
-  //           weight: "bold",
-  //         },
-  //         {
-  //           type: "text",
-  //           text: "D",
-  //           flex: 1,
-  //           size: "xxs",
-  //           weight: "bold",
-  //         },
-  //         {
-  //           type: "text",
-  //           text: "L",
-  //           flex: 1,
-  //           size: "xxs",
-  //           weight: "bold",
-  //         },
-  //         {
-  //           type: "text",
-  //           text: "+/-",
-  //           size: "xxs",
-  //           weight: "bold",
-  //           flex: 2,
-  //         },
-  //         {
-  //           type: "text",
-  //           text: "Pt",
-  //           size: "xxs",
-  //           weight: "bold",
-  //           flex: 1,
-  //         },
-  //       ],
-  //     },
-  //   ];
-  //   const data = listData.data.data;
+  else if (
+    req.body.events[0].message.type === "text" &&
+    req.body.events[0].message.text === "ตารางคะแนน"
+  ) {
+    try {
+      var listData = await axios.get(
+        "https://gaps-side-approaches-cuba.trycloudflare.com"
+      );
+    } catch (error) {
+      console.log("axios error: ", error);
+    }
+    console.log(listData.data.data);
+    createNewDataScore();
+    // Message data, must be stringified
+    const dataString = JSON.stringify({
+      replyToken: req.body.events[0].replyToken,
+      messages: [
+        {
+          type: "flex",
+          altText: "ตารางคะแนนพรีเมียร์ลีคปัจจุบัน",
+          contents: {
+            type: "bubble",
+            hero: {
+              type: "box",
+              layout: "vertical",
+              contents: [
+                {
+                  type: "image",
+                  url: "https://ga.lnwfile.com/_/ga/_raw/e2/zk/v9.png",
+                  size: "full",
+                  aspectRatio: "15:10",
+                },
+              ],
+            },
+            body: {
+              type: "box",
+              layout: "vertical",
+              contents: [
+                {
+                  type: "box",
+                  layout: "vertical",
+                  margin: "lg",
+                  spacing: "sm",
+                  contents: newDataScore,
+                },
+              ],
+            },
 
-  //   for (let i = 0; i < data.length; i++) {
-  //     number = i + 1;
-  //     let dataScore = {
-  //       type: "box",
-  //       layout: "baseline",
-  //       spacing: "sm",
-  //       contents: [
-  //         {
-  //           type: "text",
-  //           text: `${number}`,
-  //           color: "#000000",
-  //           size: "xxs",
-  //           flex: 2,
-  //         },
-  //         {
-  //           type: "icon",
-  //           url: data[i].icon,
-  //           size: "xxs",
-  //         },
-  //         {
-  //           type: "text",
-  //           text: data[i].team,
-  //           wrap: true,
-  //           color: "#666666",
-  //           size: "xxs",
-  //           flex: 3,
-  //         },
-  //         {
-  //           type: "text",
-  //           text: data[i].pi,
-  //           flex: 2,
-  //           size: "xxs",
-  //           margin: "xl",
-  //           align: "center",
-  //         },
-  //         {
-  //           type: "text",
-  //           text: data[i].w,
-  //           flex: 1,
-  //           size: "xxs",
-  //           color: "#01B54C",
-  //         },
-  //         {
-  //           type: "text",
-  //           text: data[i].d,
-  //           flex: 1,
-  //           size: "xxs",
-  //           color: "#929684",
-  //           margin: "none",
-  //         },
-  //         {
-  //           type: "text",
-  //           text: data[i].l,
-  //           flex: 1,
-  //           size: "xxs",
-  //           color: "#FA1001",
-  //           margin: "none",
-  //         },
-  //         {
-  //           type: "text",
-  //           text: data[i].gd,
-  //           flex: 2,
-  //           size: "xxs",
-  //           color: "#000000",
-  //         },
-  //         {
-  //           type: "text",
-  //           text: data[i].pts,
-  //           flex: 1,
-  //           size: "xxs",
-  //           color: "#000000",
-  //         },
-  //       ],
-  //     };
-  //     newDataScore.push(dataScore);
-  //   }
-  //   // Message data, must be stringified
-  //   const dataString = JSON.stringify({
-  //     replyToken: req.body.events[0].replyToken,
-  //     messages: [
-  //       {
-  //         type: "flex",
-  //         altText: "ตารางคะแนนพรีเมียร์ลีคปัจจุบัน",
-  //         contents: {
-  //           type: "bubble",
-  //           hero: {
-  //             type: "box",
-  //             layout: "vertical",
-  //             contents: [
-  //               {
-  //                 type: "image",
-  //                 url: "https://ga.lnwfile.com/_/ga/_raw/e2/zk/v9.png",
-  //                 size: "full",
-  //                 aspectRatio: "15:10",
-  //               },
-  //             ],
-  //           },
-  //           body: {
-  //             type: "box",
-  //             layout: "vertical",
-  //             contents: [
-  //               {
-  //                 type: "box",
-  //                 layout: "vertical",
-  //                 margin: "lg",
-  //                 spacing: "sm",
-  //                 contents: newDataScore,
-  //               },
-  //             ],
-  //           },
+            footer: {
+              type: "box",
+              layout: "vertical",
+              contents: [
+                {
+                  type: "button",
+                  action: {
+                    type: "uri",
+                    label: "ตารางคะแนนลีคอื่นๆ",
+                    uri: "https://footballline.000webhostapp.com/point.html",
+                  },
+                  color: "#6600FF",
+                  gravity: "center",
+                  style: "primary",
+                },
+              ],
 
-  //           footer: {
-  //             type: "box",
-  //             layout: "vertical",
-  //             contents: [
-  //               {
-  //                 type: "button",
-  //                 action: {
-  //                   type: "uri",
-  //                   label: "ตารางคะแนนลีคอื่นๆ",
-  //                   uri: "https://footballline.000webhostapp.com/point.html",
-  //                 },
-  //                 color: "#6600FF",
-  //                 gravity: "center",
-  //                 style: "primary",
-  //               },
-  //             ],
+              // newDataScore
+            },
+          },
+        },
+      ],
+    });
 
-  //             // newDataScore
-  //           },
-  //         },
-  //       },
-  //     ],
-  //   });
+    console.log("show data_string: ", dataString);
 
-  //   console.log("show data_string: ", dataString);
+    // Request header
+    const headers = {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + TOKEN,
+    };
 
-  //   // Request header
-  //   const headers = {
-  //     "Content-Type": "application/json",
-  //     Authorization: "Bearer " + TOKEN,
-  //   };
+    // Options to pass into the request
+    const webhookOptions = {
+      hostname: "api.line.me",
+      path: "/v2/bot/message/reply",
+      method: "POST",
+      headers: headers,
+      body: dataString,
+    };
 
-  //   // Options to pass into the request
-  //   const webhookOptions = {
-  //     hostname: "api.line.me",
-  //     path: "/v2/bot/message/reply",
-  //     method: "POST",
-  //     headers: headers,
-  //     body: dataString,
-  //   };
+    // Define request
+    const request = https.request(webhookOptions, (res) => {
+      res.on("data", (d) => {
+        process.stdout.write(d);
+      });
+    });
 
-  //   // Define request
-  //   const request = https.request(webhookOptions, (res) => {
-  //     res.on("data", (d) => {
-  //       process.stdout.write(d);
-  //     });
-  //   });
+    // Handle error
+    request.on("error", (err) => {
+      console.error(err);
+    });
 
-  //   // Handle error
-  //   request.on("error", (err) => {
-  //     console.error(err);
-  //   });
-
-  //   // Send data
-  //   request.write(dataString);
-  //   request.end();
-  // }
+    // Send data
+    request.write(dataString);
+    request.end();
+  }
 });
 
 async function handelHowToMessage(req, res,message,dataString) {
@@ -357,6 +207,118 @@ async function authoriZation(dataString){
     request.end();
   } catch (error) {
     console.log("error reply: ", error);
+  }
+}
+
+async function createNewDataScore(){
+  const newDataScore = [
+    {
+      type: "box",
+      layout: "baseline",
+      contents: [
+        { type: "text", text: "Pos.", size: "xxs", weight: "bold", flex: 2 },
+        { type: "text", text: "L", flex: 1, size: "xxs", weight: "bold" },
+        {
+          type: "text",
+          text: "Team",
+          flex: 3,
+          size: "xxs",
+          weight: "bold",
+          margin: "md",
+        },
+        {
+          type: "text",
+          text: "P",
+          flex: 2,
+          size: "xxs",
+          weight: "bold",
+          align: "center",
+        },
+        { type: "text", text: "W", flex: 1, size: "xxs", weight: "bold" },
+        { type: "text", text: "D", flex: 1, size: "xxs", weight: "bold" },
+        { type: "text", text: "L", flex: 1, size: "xxs", weight: "bold" },
+        { type: "text", text: "+/-", size: "xxs", weight: "bold", flex: 2 },
+        { type: "text", text: "Pt", size: "xxs", weight: "bold", flex: 1 },
+      ],
+    },
+  ];
+  const data = listData.data.data;
+
+  for (let i = 0; i < data.length; i++) {
+    number = i + 1;
+    let dataScore = {
+      type: "box",
+      layout: "baseline",
+      spacing: "sm",
+      contents: [
+        {
+          type: "text",
+          text: `${number}`,
+          color: "#000000",
+          size: "xxs",
+          flex: 2,
+        },
+        {
+          type: "icon",
+          url: data[i].icon,
+          size: "xxs",
+        },
+        {
+          type: "text",
+          text: data[i].team,
+          wrap: true,
+          color: "#666666",
+          size: "xxs",
+          flex: 3,
+        },
+        {
+          type: "text",
+          text: data[i].pi,
+          flex: 2,
+          size: "xxs",
+          margin: "xl",
+          align: "center",
+        },
+        {
+          type: "text",
+          text: data[i].w,
+          flex: 1,
+          size: "xxs",
+          color: "#01B54C",
+        },
+        {
+          type: "text",
+          text: data[i].d,
+          flex: 1,
+          size: "xxs",
+          color: "#929684",
+          margin: "none",
+        },
+        {
+          type: "text",
+          text: data[i].l,
+          flex: 1,
+          size: "xxs",
+          color: "#FA1001",
+          margin: "none",
+        },
+        {
+          type: "text",
+          text: data[i].gd,
+          flex: 2,
+          size: "xxs",
+          color: "#000000",
+        },
+        {
+          type: "text",
+          text: data[i].pts,
+          flex: 1,
+          size: "xxs",
+          color: "#000000",
+        },
+      ],
+    };
+    newDataScore.push(dataScore);
   }
 }
 
