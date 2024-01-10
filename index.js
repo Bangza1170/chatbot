53c9396fb2961e4b5161165f5bc6b78efd376224
Bangza1170 <101542013+Bangza1170@users.noreply.github.com>
2024-01-10 23:13:22 +0700

    Chatbot Ai refecter

--- index.js
+++ index.js
@@ -37,162 +37,8 @@ app.post("/webhook", async function (req, res) {
     }
     console.log(listData.data.data);
 
-  //  let newDataScore = createNewDataScore();
+    const newDataScore = createNewDataScore(listData);
 
-  const newDataScore = [
-    {
-      type: "box",
-      layout: "baseline",
-      contents: [
-        {
-          type: "text",
-          text: "Pos.",
-          size: "xxs",
-          weight: "bold",
-          flex: 2,
-        },
-        {
-          type: "text",
-          text: "L",
-          flex: 1,
-          size: "xxs",
-          weight: "bold",
-        },
-        {
-          type: "text",
-          text: "Team",
-          flex: 3,
-          size: "xxs",
-          weight: "bold",
-          margin: "md",
-        },
-        {
-          type: "text",
-          text: "P",
-          flex: 2,
-          size: "xxs",
-          weight: "bold",
-          align: "center",
-        },
-        {
-          type: "text",
-          text: "W",
-          flex: 1,
-          size: "xxs",
-          weight: "bold",
-        },
-        {
-          type: "text",
-          text: "D",
-          flex: 1,
-          size: "xxs",
-          weight: "bold",
-        },
-        {
-          type: "text",
-          text: "L",
-          flex: 1,
-          size: "xxs",
-          weight: "bold",
-        },
-        {
-          type: "text",
-          text: "+/-",
-          size: "xxs",
-          weight: "bold",
-          flex: 2,
-        },
-        {
-          type: "text",
-          text: "Pt",
-          size: "xxs",
-          weight: "bold",
-          flex: 1,
-        },
-      ],
-    },
-  ];
-
-    const data = listData.data.data;
-
-   // let loobNewDataScore = loobDataScore(data, newDataScore);
-
-   for (let i = 0; i < data.length; i++) {
-    number = i + 1;
-    let dataScore = {
-      type: "box",
-      layout: "baseline",
-      spacing: "sm",
-      contents: [
-        {
-          type: "text",
-          text: `${number}`,
-          color: "#000000",
-          size: "xxs",
-          flex: 2,
-        },
-        {
-          type: "icon",
-          url: data[i].icon,
-          size: "xxs",
-        },
-        {
-          type: "text",
-          text: data[i].team,
-          wrap: true,
-          color: "#666666",
-          size: "xxs",
-          flex: 3,
-        },
-        {
-          type: "text",
-          text: data[i].pi,
-          flex: 2,
-          size: "xxs",
-          margin: "xl",
-          align: "center",
-        },
-        {
-          type: "text",
-          text: data[i].w,
-          flex: 1,
-          size: "xxs",
-          color: "#01B54C",
-        },
-        {
-          type: "text",
-          text: data[i].d,
-          flex: 1,
-          size: "xxs",
-          color: "#929684",
-          margin: "none",
-        },
-        {
-          type: "text",
-          text: data[i].l,
-          flex: 1,
-          size: "xxs",
-          color: "#FA1001",
-          margin: "none",
-        },
-        {
-          type: "text",
-          text: data[i].gd,
-          flex: 2,
-          size: "xxs",
-          color: "#000000",
-        },
-        {
-          type: "text",
-          text: data[i].pts,
-          flex: 1,
-          size: "xxs",
-          color: "#000000",
-        },
-      ],
-    };
-     newDataScore.push(dataScore);
-  }
     // Message data, must be stringified
     const dataString = JSON.stringify({
       replyToken: req.body.events[0].replyToken,
@@ -251,38 +97,7 @@ app.post("/webhook", async function (req, res) {
     });
 
     console.log("show data_string: ", dataString);
-
-    // Request header
-    const headers = {
-      "Content-Type": "application/json",
-      Authorization:
-      "Bearer gpW6aqfrVCoBAyhSvPjIZoYYnOYfqYC/JhOSAXMVdYNpAtMOwf+o53maASzmQr0a8wQQTb8SEw3odehXybm7Cw2AfYzcBOqoHFWwJhKhKTzmTxSR0OOZbkA6t2gfnzaQS5w1GPjIG1pmLXRpw199agdB04t89/1O/w1cDnyilFU=",
-    };
-
-    // Options to pass into the request
-    const webhookOptions = {
-      hostname: "api.line.me",
-      path: "/v2/bot/message/reply",
-      method: "POST",
-      headers: headers,
-      body: dataString,
-    };
-
-    // Define request
-    const request = https.request(webhookOptions, (res) => {
-      res.on("data", (d) => {
-        process.stdout.write(d);
-      });
-    });
-
-    // Handle error
-    request.on("error", (err) => {
-      console.error(err);
-    });
-
-    // Send data
-    request.write(dataString);
-    request.end();
+    authoriZation(dataString);
   }
 });
 
@@ -363,7 +178,7 @@ async function authoriZation(dataString) {
   }
 }
 
-async function createNewDataScore() {
+async function createNewDataScore(listData) {
   const newDataScore = [
     {
       type: "box",
@@ -437,13 +252,11 @@ async function createNewDataScore() {
       ],
     },
   ];
-  return newDataScore;
-}
+  const data = listData.data.data;
 
-async function loobDataScore(data, newDataScore) {
-  let score = '';
   for (let i = 0; i < data.length; i++) {
     number = i + 1;
+
     let dataScore = {
       type: "box",
       layout: "baseline",
@@ -516,9 +329,9 @@ async function loobDataScore(data, newDataScore) {
         },
       ],
     };
-     newDataScore.push(dataScore);
+
+    return newDataScore.push(dataScore);
   }
-  return score;
 }
 
 app.listen(PORT, () => {
