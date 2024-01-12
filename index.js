c68ea5d7f7c6338a51e8e9a7634139dd83bacb09
Bangza1170 <101542013+Bangza1170@users.noreply.github.com>
2024-01-12 20:56:36 +0700

    Chatbot Ai refecter

--- index.js
+++ index.js
@@ -189,7 +189,7 @@ async function translateString(res, message) {
     };
 
     const response = await axios.request(options);
-    console.log("response.data" + response.data[0].translations[0].text);
+    console.log("response.data: " + response.data[0].translations[0].text);
     return res.status(200).send(response.data[0].translations[0].text);
   } catch (e) {
     console.log(e);
