cc3373502c2c0f443739f23631616fefca22eeb2
Bangza1170 <101542013+Bangza1170@users.noreply.github.com>
2024-01-13 21:55:29 +0700

    Chatbot Ai refecter

---
+++ Services/bardAuthori.js
@@ -0,0 +1,39 @@
+async function authoriZation(dataString) {
+  try {
+    const headers = {
+      "Content-Type": "application/json",
+      Authorization:
+        "Bearer gpW6aqfrVCoBAyhSvPjIZoYYnOYfqYC/JhOSAXMVdYNpAtMOwf+o53maASzmQr0a8wQQTb8SEw3odehXybm7Cw2AfYzcBOqoHFWwJhKhKTzmTxSR0OOZbkA6t2gfnzaQS5w1GPjIG1pmLXRpw199agdB04t89/1O/w1cDnyilFU=",
+    };
+
+    // Options to pass into the request
+    const webhookOptions = {
+      hostname: "api.line.me",
+      path: "/v2/bot/message/reply",
+      method: "POST",
+      headers: headers,
+      body: dataString,
+    };
+
+    // Define request
+    const request = https.request(webhookOptions, (res) => {
+      res.on("data", (d) => {
+        process.stdout.write(d);
+      });
+    });
+
+    // Handle error
+    request.on("error", (err) => {
+      console.error(err);
+    });
+
+    // Send data
+    request.write(dataString);
+    request.end();
+  } catch (error) {
+    console.log("error reply: ", error);
+  }
+}
+module.exports = {
+  authoriZation,
+};
