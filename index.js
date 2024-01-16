ff357052740c42ff4b3bcc678e4e86e0d2074acc
Bangza1170 <101542013+Bangza1170@users.noreply.github.com>
2024-01-16 23:28:52 +0700

    Chatbot Ai refecter

--- index.js
+++ index.js
@@ -21,7 +21,9 @@ app.post("/webhook", async function (req, res) {
   const message = req.body.events[0].message.text;
   var dataString = {};
   const thToEn = await translateString(message, "th", "en");
+  console.log('thToEn Data : ' +thToEn);
   const enToTh = await translateString(thToEn, "en", "th");
+  console.log('enToTh Data : ' +thToEn);
   if (enToTh.includes("คำถาม")) {
     try {
       const axios = require("axios");
