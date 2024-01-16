6ddaeeb1680f496e1c976fb0e222d28bad0c2233
Bangza1170 <101542013+Bangza1170@users.noreply.github.com>
2024-01-16 23:02:09 +0700

    Chatbot Ai refecter

--- index.js
+++ index.js
@@ -53,43 +53,43 @@ app.post("/webhook", async function (req, res) {
           ],
         });
 
-        //   try {
-        //     const headers = {
-        //       "Content-Type": "application/json",
-        //       Authorization:
-        //         "Bearer gpW6aqfrVCoBAyhSvPjIZoYYnOYfqYC/JhOSAXMVdYNpAtMOwf+o53maASzmQr0a8wQQTb8SEw3odehXybm7Cw2AfYzcBOqoHFWwJhKhKTzmTxSR0OOZbkA6t2gfnzaQS5w1GPjIG1pmLXRpw199agdB04t89/1O/w1cDnyilFU=",
-        //     };
+          try {
+            const headers = {
+              "Content-Type": "application/json",
+              Authorization:
+                "Bearer gpW6aqfrVCoBAyhSvPjIZoYYnOYfqYC/JhOSAXMVdYNpAtMOwf+o53maASzmQr0a8wQQTb8SEw3odehXybm7Cw2AfYzcBOqoHFWwJhKhKTzmTxSR0OOZbkA6t2gfnzaQS5w1GPjIG1pmLXRpw199agdB04t89/1O/w1cDnyilFU=",
+            };
 
-        //     // Options to pass into the request
-        //     const webhookOptions = {
-        //       hostname: "api.line.me",
-        //       path: "/v2/bot/message/reply",
-        //       method: "POST",
-        //       headers: headers,
-        //       body: dataString,
-        //     };
+            // Options to pass into the request
+            const webhookOptions = {
+              hostname: "api.line.me",
+              path: "/v2/bot/message/reply",
+              method: "POST",
+              headers: headers,
+              body: dataString,
+            };
 
-        //     // Define request
-        //     const request = https.request(webhookOptions, (res) => {
-        //       res.on("data", (d) => {
-        //         process.stdout.write(d);
-        //       });
-        //     });
+            // Define request
+            const request = https.request(webhookOptions, (res) => {
+              res.on("data", (d) => {
+                process.stdout.write(d);
+              });
+            });
 
-        //     // Handle error
-        //     request.on("error", (err) => {
-        //       console.error(err);
-        //     });
+            // Handle error
+            request.on("error", (err) => {
+              console.error(err);
+            });
 
-        //     // Send data
-        //     request.write(dataString);
-        //     request.end();
-        //   } catch (error) {
-        //     console.log("error reply: ", error);
-        //   }
-        // })
-        // .catch((error) => {
-        //   console.log(error);
+            // Send data
+            request.write(dataString);
+            request.end();
+          } catch (error) {
+            console.log("error reply: ", error);
+          }
+        })
+        .catch((error) => {
+          console.log(error);
       });
     } catch (error) {
       console.log("axios error: ", error);
