9b5a21a48499981f808dd2da9a50dd06e326cfe8
Bangza1170 <101542013+Bangza1170@users.noreply.github.com>
2024-01-25 20:58:05 +0700

    poc (no-card) : test cur card

--- index.js
+++ index.js
@@ -13,6 +13,42 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/guitar", (req, res) => {
+  const axios = require("axios");
+
+  let config = {
+    method: "get",
+    maxBodyLength: Infinity,
+    url: "https://graph.sanook.com/?variables=%7B%22oppaChannel%22%3A%22sport%22%2C%22oppaCategorySlugs%22%3A%5B%5D%2C%22channels%22%3A%5B%22sport%22%5D%2C%22notInCategoryIds%22%3A%5B%5D%2C%22orderBy%22%3A%7B%22field%22%3A%22CREATED_AT%22%2C%22direction%22%3A%22DESC%22%7D%2C%22first%22%3A20%2C%22keyword%22%3A%22%E0%B8%9F%E0%B8%B8%E0%B8%95%E0%B8%9A%E0%B8%AD%E0%B8%A5%22%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%2298d212b4c53a3670ec293293f18296dc5472b97ab7cbbd7b429b9aa225b284d8%22%7D%7D",
+    headers: {
+      "Accept-Language": "en-US,en;q=0.9",
+      Connection: "keep-alive",
+      Origin: "https://www.sanook.com",
+      Referer: "https://www.sanook.com/",
+      "Sec-Fetch-Dest": "empty",
+      "Sec-Fetch-Mode": "cors",
+      "Sec-Fetch-Site": "same-site",
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 OPR/106.0.0.0",
+      accept: "*/*",
+      "content-type": "application/json",
+      "sec-ch-ua":
+        '"Not_A Brand";v="8", "Chromium";v="120", "Opera GX";v="106"',
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": '"Windows"',
+    },
+  };
+
+  axios
+    .request(config)
+    .then((response) => {
+      res.status(200).json(response.data);
+    })
+    .catch((error) => {
+      res.status(400).json(error);
+    });
+});
+
 app.get("/", (req, res) => {
   res.sendStatus(200);
 });
@@ -25,7 +61,6 @@ app.post("/webhook", async function (req, res) {
 
   const thToEn = await translateString(message, "th", "en");
   // console.log('thToEn Data : ' +thToEn);
-  
 
   if (message.includes("คำถาม")) {
     try {
@@ -46,13 +81,15 @@ app.post("/webhook", async function (req, res) {
         data: data,
       };
 
-      // console.log("config Data : ", config);
-
       axios
         .request(config)
         .then(async function (response) {
           console.log("response.data : ", response.data);
-          const enToTh = await translateString(response.data.candidates[0].output, "en", "th");
+          const enToTh = await translateString(
+            response.data.candidates[0].output,
+            "en",
+            "th"
+          );
           dataString = JSON.stringify({
             replyToken: req.body.events[0].replyToken,
             messages: [
@@ -92,9 +129,8 @@ app.post("/webhook", async function (req, res) {
 
             // Send data
             request.write(dataString);
-           
-            request.end();
 
+            request.end();
           } catch (error) {
             console.log("error reply: ", error);
           }
@@ -105,6 +141,105 @@ app.post("/webhook", async function (req, res) {
     } catch (error) {
       console.log("axios error: ", error);
     }
+  } else if (message == "ข่าว") {
+    const dataString = JSON.stringify({
+      replyToken: req.body.events[0].replyToken,
+      messages: [
+        {
+          type: "carousel",
+          contents: [
+            {
+              type: "bubble",
+              body: {
+                type: "box",
+                layout: "horizontal",
+                contents: [
+                  {
+                    type: "text",
+                    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                    wrap: true,
+                  },
+                ],
+              },
+              footer: {
+                type: "box",
+                layout: "horizontal",
+                contents: [
+                  {
+                    type: "button",
+                    style: "primary",
+                    action: {
+                      type: "uri",
+                      label: "Go",
+                      uri: "https://example.com",
+                    },
+                  },
+                ],
+              },
+            },
+            {
+              type: "bubble",
+              body: {
+                type: "box",
+                layout: "horizontal",
+                contents: [
+                  {
+                    type: "text",
+                    text: "Hello, World!",
+                    wrap: true,
+                  },
+                ],
+              },
+              footer: {
+                type: "box",
+                layout: "horizontal",
+                contents: [
+                  {
+                    type: "button",
+                    style: "primary",
+                    action: {
+                      type: "uri",
+                      label: "Go",
+                      uri: "https://example.com",
+                    },
+                  },
+                ],
+              },
+            },
+          ],
+        },
+      ],
+    });
+
+    // Request header
+    const headers = {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + TOKEN,
+    };
+    // Options to pass into the request
+    const webhookOptions = {
+      hostname: "api.line.me",
+      path: "/v2/bot/message/reply",
+      method: "POST",
+      headers: headers,
+      body: dataString,
+    };
+
+    // Define request
+    const request = https.request(webhookOptions, (res) => {
+      res.on("data", (d) => {
+        process.stdout.write(d);
+      });
+    });
+
+    // Handle error
+    request.on("error", (err) => {
+      console.error(err);
+    });
+
+    // Send data
+    request.write(dataString);
+    request.end();
   }
   // else if(    req.body.events[0].message.type === "text" &&
   // req.body.events[0].message.text === "ข่าว") {
@@ -115,14 +250,14 @@ app.post("/webhook", async function (req, res) {
   //     } else {
   //       // ทำอะไรกับข้อมูล JSON ที่ได้ที่นี่
   //       const data = result.data;
-    
+
   //       // เข้าถึงข้อมูลต่าง ๆ
   //       const approval = data.$.approval;
   //       const baseUrl = data.$.base_url;
   //       const gotoEmbed = data.gotoEmbed[0];
   //       const rssDIY = data.rssDIY[0];
   //       const widgetIndex = data.widgetIndex[0];
-    
+
   //       // ตัวอย่างการเข้าถึงข้อมูลใน setting
   //       const sizeX = data.setting[0].sizeX[0];
   //       const sizeY = data.setting[0].sizeY[0];
@@ -130,23 +265,23 @@ app.post("/webhook", async function (req, res) {
   //       const theme = data.setting[0].theme[0];
   //       const topic = data.setting[0].topic[0];
   //       const totalTopic = data.setting[0].totalTopic[0];
-    
+
   //       // ตัวอย่างการเข้าถึงข้อมูลใน feedsUrl
   //       const items = data.setting[0].feedsUrl[0].item;
-    
+
   //       console.log('Approval:', approval);
   //       console.log('Base URL:', baseUrl);
   //       console.log('Goto Embed:', gotoEmbed);
   //       console.log('RSS DIY:', rssDIY);
   //       console.log('Widget Index:', widgetIndex);
-        
+
   //       console.log('Size X:', sizeX);
   //       console.log('Size Y:', sizeY);
   //       console.log('Display Type:', displayType);
   //       console.log('Theme:', theme);
   //       console.log('Topic:', topic);
   //       console.log('Total Topic:', totalTopic);
-    
+
   //       // ตัวอย่างการวนลูปเข้าถึงข้อมูลใน feedsUrl
   //       items.forEach((item, index) => {
   //         console.log(`\nItem ${index + 1}:`);
@@ -203,14 +338,14 @@ app.post("/webhook", async function (req, res) {
   //       }
   //     }
   //   };
-    
+
   //   // ตัวอย่างการใช้ Flex Message ในการส่งไปยัง Line
   //   const lineMessage = {
   //     type: 'flex',
   //     altText: 'This is a Flex Message',
   //     contents: flexMessage.contents,
   //   };
-    
+
   //   // ส่ง Flex Message ไปยัง Line
   //   axios.post('https://api.line.me/v2/bot/message/push', {
   //     to: 'userId', // แทนที่ด้วย userId ที่คุณต้องการส่ง
@@ -227,12 +362,11 @@ app.post("/webhook", async function (req, res) {
   //     .catch(error => {
   //       console.error('Error sending Flex Message:', error.response.data);
   //     });
-  // } 
-  
+  // }
   else if (
     req.body.events[0].message.type === "text" &&
-    req.body.events[0].message.text === "ตารางคะแนน") 
-    {
+    req.body.events[0].message.text === "ตารางคะแนน"
+  ) {
     try {
       var listData = await axios.get(
         "https://levels-gender-affect-owned.trycloudflare.com"
@@ -451,14 +585,11 @@ app.post("/webhook", async function (req, res) {
       ],
     });
 
-    console.log("show data_string: ", dataString);
-
     // Request header
     const headers = {
       "Content-Type": "application/json",
       Authorization: "Bearer " + TOKEN,
     };
-
     // Options to pass into the request
     const webhookOptions = {
       hostname: "api.line.me",
